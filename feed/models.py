3540aa411607db97de96a07faedd6be5263c5169
junbausa <juan_bausa@yahoo.com>
2024-01-19 14:38:08 +0800

    22. Adding authors to Posts

--- feed/models.py
+++ feed/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -8,6 +9,11 @@ class Post(models.Model):
     # auto_now = True means field is non-editable and automatically populated upon
     # data entry
     date = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.text[0:100] 
